aac5711d5f9468d176732c41cd49657b3820251e
scott pederson <scpe@users.noreply.github.com>
2023-01-20 22:24:09 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   'projectId': '4b7344',
   e2e: {},
-}
-
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'results/my-test-output.xml',
     toConsole: true,
   },
-})
+}
